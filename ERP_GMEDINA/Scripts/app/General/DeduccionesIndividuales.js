8dff77d62053c4d068409804cfd3021011f90509
Murcia7144 <jose.murcia@bi-dss.com>
2020-01-21 09:55:46 -0600

    Cambio de Validaciones a JS (Deducciones Individuales)

--- ERP_GMEDINA/Scripts/app/General/DeduccionesIndividuales.js
+++ ERP_GMEDINA/Scripts/app/General/DeduccionesIndividuales.js
@@ -143,6 +143,9 @@ $("#btnCerrarCrear").click(function () {
     $("#validation3").css("display", "none");
     $("#validation4").css("display", "none");
     $("#validation5").css("display", "none");
+    $("#validation3d").css("display", "none");
+    $("#validation4d").css("display", "none");
+    $("#validation5d").css("display", "none");
     $("#Crear #ast1").css("color", "black");
     $("#Crear #ast2").css("color", "black");
     $("#Crear #ast3").css("color", "black");
@@ -248,17 +251,18 @@ $('#btnCreateRegistroDeduccionIndividual').click(function () {
 
     //--
     if (expr.test(dei_MontoInicial)) {
-        $("#Crear #validation3").css("display", "none");
+        $("#Crear #validation3d").css("display", "none");
         $("#Crear #ast3").css("color", "black");
     }
     else {
-        $("#Crear #validation3").css("display", "");
+        $("#Crear #validation3d").css("display", "");
         $("#Crear #ast3").css("color", "red");
         TOF = false;
     }
     //////////////////////////////
     if (dei_MontoInicial == "" || dei_MontoInicial == null || dei_MontoInicial == undefined || dei_MontoInicial <= 0) {
-        $("#Crear #validation3").css("display", "block");
+        $("#Crear #validation3").css("display", "");
+        $("#Crear #validation3d").css("display", "none");
         $("#Crear #ast3").css("color", "red");
         TOF = false;
         console.log("False");
@@ -276,21 +280,24 @@ $('#btnCreateRegistroDeduccionIndividual').click(function () {
     }
     else {
         $("#Crear #validation4").css("display", "");
+        $("#Crear #validation4d").css("display", "none");
         $("#asteriscoNo4").css("color", "red");
         TOF = false;
     }
+
     if (expr.test(dei_MontoRestante)) {
-        $("#Crear #validation4").css("display", "none");
+        $("#Crear #validation4d").css("display", "none");
         $("#asteriscoNo4").css("color", "black");
     }
     else {
-        $("#Crear #validation4").css("display", "");
+        $("#Crear #validation4d").css("display", "");
         $("#Crear #ast4").css("color", "red");
         TOF = false;
     }
 
     if (dei_MontoRestante > dei_MontoInicial) {
         $("#Crear #MontoRestanteCrear").css("display", "");
+        $("#Crear #validation4d").css("display", "none");
         $("#Crear #ast4").css("color", "red");
         TOF = false;
     }
@@ -306,15 +313,17 @@ $('#btnCreateRegistroDeduccionIndividual').click(function () {
     }
     else {
         $("#Crear #validation5").css("display", "");
+        $("#Crear #validation5d").css("display", "none");
+        $("#Crear #ast5").css("color", "black");
         $("#Crear #ast5").css("color", "red");
         TOF = false;
     }
     if (expr.test(dei_Cuota)) {
-        $("#Crear #validation5").css("display", "none");
+        $("#Crear #validation5d").css("display", "none");
         $("#Crear #ast5").css("color", "black");
     }
     else {
-        $("#Crear #validation5").css("display", "");
+        $("#Crear #validation5d").css("display", "");
         $("#Crear #ast5").css("color", "red");
         TOF = false;
     }
@@ -416,7 +425,10 @@ $("#btnCerrarEditar").click(function () {
     $("#validatione1").css("display", "none");
     $("#validatione3").css("display", "none");
     $("#validatione4").css("display", "none");
-    $("#validatione4").css("display", "none");
+    $("#validatione5").css("display", "none");
+    $("#validatione3d").css("display", "none");
+    $("#validatione4d").css("display", "none");
+    $("#validatione5d").css("display", "none");
     $("#Editar #aste1").css("color", "black");
     $("#Editar #aste2").css("color", "black");
     $("#Editar #aste3").css("color", "black");
@@ -508,7 +520,8 @@ $("#btnEditDeduccionIndividual").click(function () {
     //--
 
     if (vale3 == "" || vale3 == null || vale3 == undefined || vale3 <= 0) {
-        $("#Editar #validatione3").css("display", "block");
+        $("#Editar #validatione3").css("display", "");
+        $("#Editar #validatione3d").css("display", "none");
         $("#Editar #3editaste").css("color", "red");
         TOF = false;
     }
@@ -516,11 +529,11 @@ $("#btnEditDeduccionIndividual").click(function () {
         $("#Editar #validatione3").css("display", "none");
         $("#Editar #3editaste").css("color", "black");
         if (expr.test(vale3)) {
-            $("#Editar #validatione3").css("display", "none");
+            $("#Editar #validatione3d").css("display", "none");
             $("#Editar #3editaste").css("color", "black");
         }
         else {
-            $("#Editar #validatione3").css("display", "");
+            $("#Editar #validatione3d").css("display", "");
             $("#Editar #3editaste").css("color", "red");
             TOF = false;
         }
@@ -529,6 +542,7 @@ $("#btnEditDeduccionIndividual").click(function () {
 
     if (vale4 > vale3) {
         $("#Editar #MontoRestanteEditar").css("display", "");
+        $("#Editar #validatione4d").css("display", "none");
         $("#Editar #4editaste").css("color", "red");
         TOF = false;
     }
@@ -543,15 +557,16 @@ $("#btnEditDeduccionIndividual").click(function () {
     }
     else {
         $("#Editar #validatione4").css("display", "");
+        $("#Editar #validatione4d").css("display", "none");
         $("#Editar #4editaste").css("color", "red");
         TOF = false;
     }
     if (expr.test(vale4)) {
-        $("#Editar #validatione4").css("display", "none");
+        $("#Editar #validatione4d").css("display", "none");
         $("#Editar #4editaste").css("color", "black");
     }
     else {
-        $("#Editar #validatione4").css("display", "");
+        $("#Editar #validatione4d").css("display", "");
         $("#Editar #4editaste").css("color", "red");
         TOF = false;
     }
@@ -562,15 +577,16 @@ $("#btnEditDeduccionIndividual").click(function () {
     }
     else {
         $("#Editar #validatione5").css("display", "");
+        $("#Editar #validatione5d").css("display", "none");
         $("#Editar #5editaste").css("color", "red");
         TOF = false;
     }
     if (expr.test(vale5)) {
-        $("#Editar #validatione5").css("display", "none");
+        $("#Editar #validatione5d").css("display", "none");
         $("#Editar #5editaste").css("color", "black");
     }
     else {
-        $("#Editar #validatione5").css("display", "");
+        $("#Editar #validatione5d").css("display", "");
         $("#Editar #5editaste").css("color", "red");
         TOF = false;
     }
